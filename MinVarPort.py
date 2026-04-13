fa40639ec5050993b17fae4cf64a7f5357283ff3
unclebobatwork14-max <unclebobatwork14@gmail.com>
2026-04-13 17:34:17 +0100

    Update MinVarPort.py

--- MinVarPort.py
+++ MinVarPort.py
@@ -629,8 +629,7 @@ def render_theoretical_tab(
     ax2.scatter([mvp_esg["Std Dev"] * 100], [mvp_esg["Expected Return"] * 100], color=ESG_GREEN, s=45, zorder=4)
 
     ax2.annotate(
-        "tangency portfolio
-(all assets)",
+        "tangency portfolio/n (all assets)",
         xy=(tan_std["Std Dev"] * 100, tan_std["Expected Return"] * 100),
         xytext=(-80, 10),
         textcoords="offset points",
